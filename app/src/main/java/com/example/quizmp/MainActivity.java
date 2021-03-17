34592a5db05357faf793299a9c11dabbeee164d8
kaleb <kalebclement19@gmail.com>
2021-03-17 16:59:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quizmp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.quizmp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) this.findViewById(R.id.mywidget);
+        tv.setSelected(true);
+    }
+}
