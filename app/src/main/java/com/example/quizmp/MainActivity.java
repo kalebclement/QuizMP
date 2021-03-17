a843f9b231d30a937ce92d0e1b0de3cc879226b5
kaleb <kalebclement19@gmail.com>
2021-03-17 19:14:58 +0700

    second

--- app/src/main/java/com/example/quizmp/MainActivity.java
+++ app/src/main/java/com/example/quizmp/MainActivity.java
@@ -7,13 +7,16 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tv;
+    private TextView runningtxt1, runningtxt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) this.findViewById(R.id.mywidget);
-        tv.setSelected(true);
+        runningtxt1 = (TextView) this.findViewById(R.id.runningText1);
+        runningtxt1.setSelected(true);
+        runningtxt2 = (TextView) this.findViewById(R.id.runningText2);
+        runningtxt2.setSelected(true);
+
     }
 }
